1f2583ef8e1a81a94b6f3c85cc3c952db397eb87
AlfredEsch <alfred.eschbaum@khk.ee>
2022-09-20 11:28:48 +0300

    variables

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 console.log("Hello World!")
 
+// data types
+// string
+
 // variables
 let name = "Alfred Eschbaum"
 name = "new name"
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(percent)
+console.log(typeof percent)
+
+let isHere = true //false
+console.log(isHere)
+console.log(typeof isHere)
+
 
 // const
 const surname = "Eschbaum"
@@ -24,4 +42,19 @@ console.log(person.firstname)
 console.log(person.surname)
 person.age = '17'
 console.log(person.age)
-//
+
+// Null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+//symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+//undefined
+
+let test
+console.log(test)
+console.log(typeof test)
