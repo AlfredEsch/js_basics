d72a8da832aed0dd52f804c4894eea45738f2460
AlfredEsch <alfred.eschbaum@khk.ee>
2022-09-20 11:18:09 +0300

    variables

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello World!")
+
+// variables
+let name = "Alfred Eschbaum"
+name = "new name"
+
+// const
+const surname = "Eschbaum"
+// surname = "Kork" - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Alfred',
+    surname: 'Eschbaum'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.age = '17'
+console.log(person.age)
