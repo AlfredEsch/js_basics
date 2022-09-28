2f7560f0fcd9f132be8487fd8f64be2c0fec6e70
AlfredEsch <alfred.eschbaum@khk.ee>
2022-09-28 10:55:06 +0300

    DOM Sündmused ehk event

---
+++ syndmused.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskHeading = document.querySelector('#task-title')
+
+// submit
+form.addEventListener('submit', runEvent)
+
+//keyboard
+// taskInput.addEventListener('keydown', runEvent)
+// taskInput.addEventListener('keyup', runEvent)
+// taskInput.addEventListener('keypress', runEvent)
+
+// focus
+// taskInput.addEventListener('focus', runEvent)
+// taskInput.addEventListener('blur', runEvent)
+
+// input
+// taskInput.addEventListener('cut', runEvent)
+// taskInput.addEventListener('paste', runEvent)
+// taskInput.addEventListener('input', runEvent)
+
+
+function  runEvent(event){
+    console.log(`event type: ${event.type}`)
+    taskHeading.innerText = event.target.value
+    // event.preventDefault()
+}
+
